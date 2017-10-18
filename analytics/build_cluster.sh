d40f14d4d4fe1457c0bd17f180a87ec2658eb8d8
Chris Hillery <ceej@couchbase.com>
2017-10-18 12:23:55 -0700

    MB-25842: Advance build; move tlm to new analytics-dp branch
    Change-Id: I129f067f1420e3263fb453c803dee3151089875b
    Reviewed-on: http://review.couchbase.org/84532
    Reviewed-by: Till Westmann <till@couchbase.com>
    Tested-by: Build Bot <build@couchbase.com>

--- analytics/build_cluster.sh
+++ analytics/build_cluster.sh
@@ -2,7 +2,7 @@
 
 if [ -z "$1" ]; then
   echo Usage: $0 \<manifest url\>
-  echo e.g. $0 http://172.23.120.24/builds/latestbuilds/couchbase-server/spock/3217/couchbase-server-5.0.0-3217-manifest.xml
+  echo e.g. $0 http://172.23.120.24/builds/latestbuilds/couchbase-server/spock/3519/couchbase-server-5.0.0-3519-manifest.xml
   exit 1
 fi
 
@@ -14,6 +14,7 @@ fi
   echo "     ##### -->"
   curl $1 | awk -v regex="RELEASE.*spock" -v count="3" '$0 ~ regex { skip=count; next } --skip >= 0 { next } 1' \
       | awk -v regex="name=\"tlm\"" -v count="5" '$0 ~ regex { skip=count; next } --skip >= 0 { next } 1' \
+      | awk -v regex="name=\"build\"" -v count="5" '$0 ~ regex { skip=count; next } --skip >= 0 { next } 1' \
       | sed -e '/name="voltron"/d' -e '/name="ns_server"/d' -e '/name="couchbase-cli"/d' -e '/name="query"/d' \
             -e '/name="testrunner"/d' -e '/"@.*@"/d' -e '$d'
   echo
